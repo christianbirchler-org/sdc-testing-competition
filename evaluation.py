e0e6b5a839a3fccba908988f94d7b886afcc4bd5
Christian Birchler <birchler.chr@gmail.com>
2024-08-09 16:34:56 +0200

    layout basic evaluation structure

--- evaluation.py
+++ evaluation.py
@@ -1,5 +1,33 @@
-class ToolEvaluator:
+from interfaces import TestSelector, SDCTest
+from tools.sample_tool.sample_test_selector import SampleTestSelector
+
+
+class MetricEvaluator:
+    def __init__(self):
+        pass
+
+    def bug_time_ratio(self, test_suite: list[SDCTest], selection: list[bool]) -> float:
+        return 0.0
+
+
+class EvaluationReport:
     pass
+
+class ToolEvaluator:
+    def __init__(self, test_suite: list[SDCTest], metric_evaluator: MetricEvaluator):
+        self.test_suite = test_suite
+        self.metric_evaluator = metric_evaluator
+
+    def evaluate(self, tool: TestSelector):
+        tool.initialize(self.test_suite)
+        selection = tool.select(self.test_suite)
+        self.metric_evaluator.bug_time_ratio(self.test_suite, selection)
+
 
 if __name__ == '__main__':
     print('start evaluation')
+    te = ToolEvaluator([SDCTest(), SDCTest()], MetricEvaluator())
+    ts = SampleTestSelector()
+
+    te.evaluate(ts)
+
