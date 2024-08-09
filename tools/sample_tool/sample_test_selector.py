26168349abb0de2cf3a12d9d7eba7701b29b1b37
Christian Birchler <birchler.chr@gmail.com>
2024-08-09 16:34:32 +0200

    provide dummy implementation

--- tools/sample_tool/sample_test_selector.py
+++ tools/sample_tool/sample_test_selector.py
@@ -4,5 +4,8 @@ from interfaces import TestSelector, SDCTest
 
 
 class SampleTestSelector(TestSelector):
+    def initialize(self, test_suite: list[SDCTest]) -> None:
+        pass
+
     def select(self, test_suite: list[SDCTest]) -> list[bool]:
         return [random.random() < 0.5 for _ in test_suite]
