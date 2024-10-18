f92b32dec4829fd3cb5be2aa21656a26ee0414c3
Christian Birchler <birchler.chr@gmail.com>
2024-10-18 16:14:27 +0200

    feat: provide diversity metric based on curvature
    The metric is based on curvature profiles. The idea is to compute for
    every test case the curvature profile. For every meter of road the
    curvature is approximated. From the profile of a test case we determine
    the mean and standard deviation of the curvature profile.
    
    The diversity of the whole selected set of test cases is computed as
    follows: first, we compute the pairwise distance of the test cases mean
    and std of the associating curvature profile. secondly, we compute the
    mean of those distances.
    
    refs: #6

--- evaluator/evaluation.py
+++ evaluator/evaluation.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from sklearn.metrics.pairwise import pairwise_distances
 import numpy as np
 import shapely
 import time
@@ -32,7 +33,7 @@ def _curvature_profile(test_detail: TestDetails) -> list[float]:
     print("compute curvature profile")
     road_shape = shapely.LineString(test_detail.road_points)
 
-    delta_s = 3  # 3 meters
+    delta_s = 2  # 10 meters
 
     curvature_profile = np.zeros(int(road_shape.length)) # we want the curvature for every meter
     for s in range(len(curvature_profile)):
@@ -110,14 +111,26 @@ class MetricEvaluator:
 
     def diversity(self, test_suite: list[TestDetails], selection: list[str]) -> float:
         """
-        diversity of the selected test cases
+        TODO: Diversity of the selected test cases
+        Current implementation is not final!!!
+        We might consider different definitions of diversity.
         """
-        curvature_profiles = []
+        curvature_profiles_stats = []
 
         for test_detail in test_suite:
             if test_detail.test_id in selection:
-                curvature_profiles.append(_curvature_profile(test_detail))
-        return curvature_profiles
+                profile = _curvature_profile(test_detail)
+                profile_stat = {
+                    'k_mean': np.mean(profile),
+                    'k_std': np.std(profile)
+                }
+                pt = [profile_stat['k_mean'], profile_stat['k_std']]
+
+                curvature_profiles_stats.append(pt)
+
+        curvature_stats_distances = pairwise_distances(curvature_profiles_stats, curvature_profiles_stats)
+        #return curvature_profiles_stats
+        return float(np.mean(curvature_stats_distances[0, :]))
 
 
 @dataclass
